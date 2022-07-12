cd3501dcb40e3485759a6f4bee5be1d5147d3648
sound-round <yudaev1@gmail.com>
2022-07-12 22:19:11 +0300

    Pickup weapon and some fixes

--- my_strategy.py
+++ my_strategy.py
@@ -1,3 +1,4 @@
+import muffin
 from debugging.color import Color
 from model.game import Game
 from model.order import Order
@@ -21,30 +22,44 @@ class MyStrategy:
             if unit.player_id != game.my_id:
                 continue
             # TODO: 1. оборачиваться на звуки 2. Уворачиваться 
-            # 3. стрелять с расстояния выстрела
+            # 3. стрелять с расстояния выстрела 4. Не стрелять через стены, которые не простреливаются
+            # TODO: queue = [], куда записывать очередность действий
 
+            # sounds = game.sounds
             my_unit = unit
             velocity = Vec2(-unit.position.x, -unit.position.y)
             direction = Vec2(-unit.direction.y, unit.direction.x)  # просто крутится взглядом
             shoot = False
+
+            max_distance_to_enemy = 30
+            if my_unit.weapon == 2:
+                max_distance_to_enemy = 40
+            if my_unit.weapon == 1:
+                max_distance_to_enemy = 20
+
             ranked_enemies = self.sort_enemies(my_unit, game.units, game)
+
 
             loot_item = None
             possibleTarget = None
-            if my_unit.shield_potions <= 1:
+            if my_unit.shield_potions == 0:
                 loot_item = self.find_shield_potion(game.loot, my_unit)
+            if not loot_item and my_unit.weapon == 0:
+                loot_item = self.find_weapon(game.loot, my_unit)
             if not loot_item and my_unit.ammo[my_unit.weapon] == 0:
                 loot_item = self.find_ammo(game.loot, my_unit)
-            if not loot_item:
-                possibleTarget = self.find_enemy(ranked_enemies)
+            # if not loot_item:
+            possibleTarget = self.find_enemy(ranked_enemies)
             if possibleTarget:
                 direction = possibleTarget.position.copy().minus(unit.position)
                 velocity = direction
                 distance = self.find_distance(my_unit, possibleTarget)
+
                 if distance < SHOOT_DISTANCE_TO_ENEMY:
                     velocity = Vec2(0, 0)
-
-                shoot = True
+                
+                if distance < max_distance_to_enemy:
+                    shoot = True 
 
                 if debug_interface:
                     self.show_target(debug_interface, unit, possibleTarget)
@@ -54,7 +69,6 @@ class MyStrategy:
             if loot_item:
                 distance = self.find_distance(loot_item, my_unit)
                 if distance < 1:
-
                     action = ActionOrder.Pickup(loot_item.id)
                 direction = loot_item.position.copy().minus(unit.position)
                 velocity = Vec2(direction.x * 2000, direction.y * 2000)
@@ -62,11 +76,12 @@ class MyStrategy:
                 if debug_interface:
                         self.show_target(debug_interface, unit, loot_item)
 
-            if my_unit.shield < 60 and my_unit.shield_potions and not action:
-                action = ActionOrder.UseShieldPotion()
-            
-            action = action or ActionOrder.Aim(shoot)
+            if not action and shoot:
+                action = ActionOrder.Aim(shoot)
 
+            if my_unit.shield < 90 and my_unit.shield_potions and not action and not shoot:
+                action = ActionOrder.UseShieldPotion()
+            print(action)
             orders[unit.id] = UnitOrder(
                 velocity,
                 direction,
@@ -107,6 +122,17 @@ class MyStrategy:
             return
         ammos.sort()
         return ammos[0][1]
+
+    def find_weapon(self, loot, my_unit):
+        weapons = []
+        for obj in loot:
+            if obj.item.TAG == 0 and obj.item.type_index == 1:
+                distance = self.find_distance(obj, my_unit)
+                weapons.append((distance, obj))
+        if not weapons:
+            return
+        weapons.sort()
+        return weapons[0][1]
 
     def sort_enemies(self, my_unit, units, game):
         enemies = []
