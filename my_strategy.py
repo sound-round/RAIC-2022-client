ef127f7b0a1faf34babf82fb94de73d0c12a6975
sound-round <yudaev1@gmail.com>
2022-07-17 20:50:11 +0300

    Update strategy

--- my_strategy.py
+++ my_strategy.py
@@ -35,13 +35,15 @@ class MyStrategy:
             if unit.player_id != game.my_id:
                 continue
             # TODO: 
+            # - щите 200, а не 100 ед.
+            # - сделать машину состояний (должен быть большой буст)
             # - оборачиваться на звуки 
             # - учесть минимальное расстояние до противника (с учетом его оружия)
             # - Диман: короч я сделал запоминание лута\снарядов (можно и врагов) невидимых
             #           - и это тоже буст хороший похоже
             # - Сделать ранжирование опасности для подъема предмета
             # - Не стрелять через стены, которые не простреливаются
-            # - зона сужается, нужно учесть (не собирать лут, бежать в центр безопасной зоны)!
+            # - зона сужается, нужно учесть (не собирать лут, бежать в центр безопасной зоны)! большой буст!
             # - queue = [], куда записывать очередность действий
 
             # FIXME:
@@ -54,11 +56,14 @@ class MyStrategy:
             direction = Vec2(-unit.direction.y, unit.direction.x)  # просто крутится взглядом
             shoot = False
 
+            
             max_distance_to_enemy = 30
             if my_unit.weapon == 2:
                 max_distance_to_enemy = 40
             if my_unit.weapon == 1:
                 max_distance_to_enemy = 20
+
+            min_distance_to_enemy = min(max_distance_to_enemy, 25)
 
             ranked_enemies = self.sort_enemies(my_unit, game.units, game)
 
@@ -75,44 +80,52 @@ class MyStrategy:
             possibleTarget = self.find_enemy(ranked_enemies)
             # distance_to_possible_target = 300
             if possibleTarget:
-                min_distance_to_target = 22
+                
                 if possibleTarget.weapon == 0:
-                    min_distance_to_target = 32
+                    min_distance_to_enemy = min(32, max_distance_to_enemy)
 
                 distance = self.find_distance(my_unit, possibleTarget)
                 # distance_to_possible_target = distance
                 # direction = possibleTarget.position.copy().minus(unit.position)
                 # velocity = direction
-                velocity = Vec2(direction.x + self.random_xy[0], direction.y + self.random_xy[1])
+                # velocity = Vec2(direction.x + self.random_xy[0], direction.y + self.random_xy[1])
                 
 
                 # if distance < SHOOT_DISTANCE_TO_ENEMY:
                 #     velocity = Vec2(0, 0)
-                
-                if distance < max_distance_to_enemy and my_unit.ammo[my_unit.weapon]:
-                    direction = possibleTarget.position.copy().minus(unit.position).plus_vector(possibleTarget.velocity)
-                    velocity = Vec2(direction.x + self.random_xy[0], direction.y + self.random_xy[1])
-                    shoot = True 
+                if my_unit.ammo[my_unit.weapon] and distance <= max_distance_to_enemy:
+                    shoot = True
                     action = ActionOrder.Aim(shoot)
+                direction = possibleTarget.position.copy().minus(unit.position).plus_vector(possibleTarget.velocity)
+                if min_distance_to_enemy < distance < max_distance_to_enemy:
+                    velocity = Vec2(direction.y + self.random_xy[1], - direction.x + self.random_xy[0])
+                if  distance > max_distance_to_enemy:
+                    velocity = Vec2(direction.x + self.random_xy[0], direction.y + self.random_xy[1])
+                if distance < min_distance_to_enemy:
+                    
+                    # current_vector = possibleTarget.position.copy().minus(unit.position)
+                    velocity = Vec2(- direction.x + self.random_xy[0], -direction.y + self.random_xy[1])
+
                 if debug_interface:
                     self.show_target(debug_interface, unit, possibleTarget)
 
             
 
-            if loot_item:# and distance_to_possible_target > 30 or not my_unit.ammo[my_unit.weapon]:
+            if loot_item and not shoot:# and distance_to_possible_target > 30 or not my_unit.ammo[my_unit.weapon]:
                 # shoot = False
                 if loot_item.item.TAG == 2:
                     print(loot_item.item.amount)
                 distance = self.find_distance(loot_item, my_unit)
                 if distance < 1:
                     action = ActionOrder.Pickup(loot_item.id)
+                # if not possibleTarget:
                 direction = loot_item.position.copy().minus(unit.position)
                 velocity = Vec2(direction.x * 2000, direction.y * 2000)
 
                 if debug_interface:
                         self.show_target(debug_interface, unit, loot_item)
 
-            if my_unit.shield < 90 and my_unit.shield_potions and not action and not shoot:
+            if my_unit.shield < 100 and my_unit.shield_potions and not action and not shoot:
                 action = ActionOrder.UseShieldPotion()
             orders[unit.id] = UnitOrder(
                 velocity,
